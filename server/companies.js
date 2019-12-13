abe2f828ad84e293dcd131163af3f3ae3f46633f
Mike Mols <mm@firtal.com>
2019-12-13 21:33:24 +0100

    fix: unique ids

--- server/companies.js
+++ server/companies.js
@@ -1,32 +1,34 @@
+const uuidv4 = require("./helpers");
+
 const companies = [
-    {
-        id: Date.now(),
-        title: 'Firtal',
-        started_at: 'Nov 2017',
-        ended_at: 'Present',
-        position: 'Software Developer',
-    },
-    {
-        id: Date.now(),
-        title: 'Nobrainer Web',
-        started_at: 'May 2017',
-        ended_at: 'Jul 2017',
-        position: 'Frontend Developer',
-    },
-    {
-        id: Date.now(),
-        title: 'Misena ApS',
-        started_at: 'Aug 2016',
-        ended_at: 'Nov 2016',
-        position: 'Web Developer',
-    },
-    {
-        id: Date.now(),
-        title: 'Ideaweb ApS',
-        started_at: 'Jan 2015',
-        ended_at: 'Mar 2015',
-        position: 'Web Developer',
-    }
+  {
+    id: uuidv4(),
+    title: "Firtal",
+    started_at: "Nov 2017",
+    ended_at: "Present",
+    position: "Software Developer"
+  },
+  {
+    id: uuidv4(),
+    title: "Nobrainer Web",
+    started_at: "May 2017",
+    ended_at: "Jul 2017",
+    position: "Frontend Developer"
+  },
+  {
+    id: uuidv4(),
+    title: "Misena ApS",
+    started_at: "Aug 2016",
+    ended_at: "Nov 2016",
+    position: "Web Developer"
+  },
+  {
+    id: uuidv4(),
+    title: "Ideaweb ApS",
+    started_at: "Jan 2015",
+    ended_at: "Mar 2015",
+    position: "Web Developer"
+  }
 ];
 
 module.exports = companies;
